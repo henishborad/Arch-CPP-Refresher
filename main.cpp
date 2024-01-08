174a510852faded08ac2bdecaf996a5093e991ab
Henish Borad <henishborad@gmail.com>
2024-01-07 21:45:37 -0800

    Update

--- main.cpp
+++ main.cpp
@@ -7,19 +7,19 @@ int main() {
     a = cin.get();
     cout << a;
 
-    // int a, b;
-    // cout << "Enter a: ";
-    // cin >> a;
-    // cout << "Enter b: ";
-    // cin >> b;
-    //
-    // if (a > b) {
-    //     cout << "a is greater!";
-    // }
-    // else if (b > a) {
-    //     cout << "b is greater!";
-    // }
-    // else {
-    //     cout << "equal equal!";
-    // }
+    int a, b;
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
+
+    if (a > b) {
+        cout << "a is greater!";
+    }
+    else if (b > a) {
+        cout << "b is greater!";
+    }
+    else {
+        cout << "equal equal!";
+    }
 }

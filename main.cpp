24c42464952373dac26b2b4ba555a92238a10da6
Henish Borad <henishborad@gmail.com>
2024-01-07 21:45:50 -0800

    Update

--- main.cpp
+++ main.cpp
@@ -1,25 +1,25 @@
-#include "bits/stdc++.h"
-using namespace std;
-
-int main() {
-
-    string a;
-    a = cin.get();
-    cout << a;
-
-    int a, b;
-    cout << "Enter a: ";
-    cin >> a;
-    cout << "Enter b: ";
-    cin >> b;
-
-    if (a > b) {
-        cout << "a is greater!";
-    }
-    else if (b > a) {
-        cout << "b is greater!";
-    }
-    else {
-        cout << "equal equal!";
-    }
-}
+// #include "bits/stdc++.h"
+// using namespace std;
+//
+// int main() {
+//
+//     string a;
+//     a = cin.get();
+//     cout << a;
+//
+//     int a, b;
+//     cout << "Enter a: ";
+//     cin >> a;
+//     cout << "Enter b: ";
+//     cin >> b;
+//
+//     if (a > b) {
+//         cout << "a is greater!";
+//     }
+//     else if (b > a) {
+//         cout << "b is greater!";
+//     }
+//     else {
+//         cout << "equal equal!";
+//     }
+// }
